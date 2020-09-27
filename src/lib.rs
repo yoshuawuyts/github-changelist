797d5e7cdf96219a08295d5fc33ef28882b34c9e
Yoshua Wuyts <yoshuawuyts@gmail.com>
2020-09-27 17:27:04 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! Get the list of merged PRs on GitHub since the last release
+//!
+//! # Examples
+//!
+//! ```
+//! // tbi
+//! ```
+
+#![forbid(unsafe_code, future_incompatible, rust_2018_idioms)]
+#![deny(missing_debug_implementations, nonstandard_style)]
+#![warn(missing_docs, missing_doc_code_examples, unreachable_pub)]
